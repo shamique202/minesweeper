5e0629e9dca8e98ab3a2a7df09093d2c5a25274b
shamique202 <shamique.e@gmail.com>
2022-03-22 14:27:52 -0400

    added variables/elements

--- js/main.js
+++ js/main.js
@@ -1,17 +1,50 @@
-// myconstants
-const sizeBoarding = {
+//myconstants!!!
+let sizeBoarding = {
     's': 9,
     'l': 17
 }
 
-const numberMines = {
-        's': 11,
-        'l': 40,
-    }
-    // app's state variables 
+let numberMines = {
+    's': 11,
+    'l': 40,
+}
+
+//my app's state variables!!!
 let theBoard;
 let theSeconds = 0;
 let theInterval = null;
 let theSize;
 let newGames = true;
 let finishedGame = false;
+
+//my cached elements!!!
+let boardGameEl = document.getElementById('boardGame');
+let h3El = document.querySelector('h3');
+let minestoLeftEl = document.getElementById('minestoLeftEl');
+let timerEl = document.getElementById('timer');
+let easyDifEl = document.getElementById('s');
+let hardDifEl = document.getElementById('l');
+let myButtonsEl = document.getElementById('mybuttons');
+
+//my eventlisteners!!!!
+boardGameEl.addEventListener('click', handleClick);
+boardGameEl.addEventListener('contextmenu', handler);
+easyDifEl.addEventListener('click', init);
+hardDifEl.addEventListener('click', init);
+document.getElementById('reset').addEventListener('click', resetBtn);
+
+//my functions below!!!
+function init(e) {
+    size = e.target.id;
+    finishedGame = false;
+
+    styleboardGame(size);
+    createBoardGameDivs(size);
+    createBoardGameArray(size);
+}
+
+//Whichever level a player chooses, the game changes from easy to hard / the level changes
+function styleboardGame(size) {
+    myButtonsEl.style.display = 'none';
+    boardGameEl.innerHTML = '';
+}
