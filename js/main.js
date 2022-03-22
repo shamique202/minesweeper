7fc066f282f3af5a9b811f9f2384b6c8d54467d8
shamique202 <shamique.e@gmail.com>
2022-03-22 18:49:45 -0400

    loops

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-//myconstants!!!
+//myconstants!!!!
 let sizeBoarding = {
     's': 9,
     'l': 17
@@ -20,7 +20,7 @@ let finishedGame = false;
 //my cached elements!!!
 let boardGameEl = document.getElementById('boardGame');
 let h3El = document.querySelector('h3');
-let minestoLeftEl = document.getElementById('minestoLeftEl');
+let minestoLeftEl = document.getElementById('minestoLeft');
 let timerEl = document.getElementById('timer');
 let easyDifEl = document.getElementById('s');
 let hardDifEl = document.getElementById('l');
@@ -47,4 +47,41 @@ function init(e) {
 function styleboardGame(size) {
     myButtonsEl.style.display = 'none';
     boardGameEl.innerHTML = '';
+
+    boardGameEl.style.backgroundColor = '#FABDD8';
+    boardGameEl.style.border = '3px solid black';
+    boardGameEl.style.display = 'grid';
+    boardGameEl.style.marginTop = '39px';
+    boardGameEl.style.textAlign = 'center';
+    boardGameEl.style.justifyContent = 'center';
+    boardGameEl.style.margin = '31px auto';
+
+    if (size === 's') {
+        boardGameEl.style.width = '245px';
+        boardGameEl.style.gridTemplateColumns = 'repeat(9, 27px)';
+        boardGameEl.style.gridTemplateRows = 'repeat(9, 27px)';
+        h3El.innerText = 'good job!';
+    }
+    if (size === 'l') {
+        boardGameEl.style.width = '433px';
+        boardGameEl.style.gridTemplateColumns = 'repeat(16, 27px)';
+        boardGameEl.style.gridTemplateRows = 'repeat(16, 27px)';
+        h3El.innerText = `...you're gonna need it`
+    }
+    minestoLeftEl.innerText = numberMines[size];
+    timerEl.innerText = `00:00`;
+}
+//make boardelements in the d.o.m.
+funtion createBoardGameDivs(size) {
+    for (let i = 0; i < sizeBoarding[size]; i++) {
+        for (let j = 0; j < sizeBoarding[size]; j++) {
+            let newDiv = document.createElement('div');
+            newDiv.id = `c${j}r${i}`;
+            newDiv.style.border = '1px pink';
+            newDiv.style.borderTopColor = 'white';
+            newDiv.style.borderLeftColor = 'white';
+            newDiv.style.fontSize = '20px';
+            boardGameEl.appendChild(newDiv);
+        }
+    }
 }
