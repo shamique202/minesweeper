59c8d5c4be5544e9b9909111d138e6df76735ded
shamique202 <shamique.e@gmail.com>
2022-03-23 21:14:19 -0400

    flags

--- js/main.js
+++ js/main.js
@@ -292,6 +292,23 @@ function handleClick(e) {
             //the winner is announced!
             clearInterval(interval);
             h3El.innerText = 'You might want to try a little harder next time, just saying...';
+        } else {
+            render(col, row);
+            checkWinner();
         }
     }
+}
+
+function handleRghtClick(e) {
+    const ind = (e.target.id).indexOf('r');
+    let col = parseInt((e.target.id).substring(1, ind));
+    let row = parseInt((e.target.id).substring(ind + 1, (e.target.id).length));
+
+    let squareEl = document.getElementById(`c${col}r${row}`);
+
+
+
+
+
+
 }
