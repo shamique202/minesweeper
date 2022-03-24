6cf3ac0b41d22b35111adb7d0e39afc7abd27053
shamique202 <shamique.e@gmail.com>
2022-03-24 15:00:00 -0400

    updated readme

--- js/main.js
+++ js/main.js
@@ -368,7 +368,3 @@ function formatTime() {
     const secs = (theSeconds % 60).toString().padStart(2, '0');
     timerEl.innerText = `${mins}:${secs}`;
 }
-
-//explosions sound
-var sound = new Audio("audio_235968__tommccann__explosion-01.wav");
-sound.play();
